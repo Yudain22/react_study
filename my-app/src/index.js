8ddeabff0bdb68b124da1015672551cc0866c918
Yudain22 <di4046@gmail.com>
2024-07-05 11:29:41 +0900

    댓글 컴포넌트 실습

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,17 +6,19 @@ import reportWebVitals from './reportWebVitals';
 
 import Library from './03.JSX소개/Library';
 import Clock from './04.엘리먼트 렌더링/Clock';
+import CommentList from './05.컴포넌트와 Props/CommentList';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-setInterval(()=> {
+// setInterval(()=> {
   root.render(
     <React.StrictMode>
       {/* <App /> */}
       {/* <Library/> */}
-      <Clock/>
+      {/* <Clock/> */}
+      <CommentList/>
     </React.StrictMode>
   );
-},1000);
+// },1000);
 
 
 // If you want to start measuring performance in your app, pass a function
