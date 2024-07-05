6a5965a49b618c56f0aca5962a8b3b4d2bf55566
Yudain22 <di4046@gmail.com>
2024-07-05 11:52:54 +0900

    컴포넌트 데이터 별도 객체로 분리

--- my-app/src/05.컴포넌트와 Props/CommentList.jsx
+++ my-app/src/05.컴포넌트와 Props/CommentList.jsx
@@ -2,11 +2,38 @@ import React from "react";
 import Comment from "./Comment";
 
 //props로 데이터 넣기
+// function CommentList(props) {
+//     return (
+//         <div>
+//             <Comment name={"유다인"} comment={"나는 유다인이다!"}/>
+//             <Comment name={"백승엽"} comment={"나는 백승여비다!"}/>
+//         </div>
+//     );
+// }
+
+const comments = [
+    {
+        name:"유다인",
+        comment:"저는 유다인입니다."
+    },
+    {
+        name:"백승엽",
+        comment:"저는 백승엽입니다."
+    },
+    {
+        name:"유다연",
+        comment:"저는 유다연입니다."
+    },
+];
+
 function CommentList(props) {
     return (
         <div>
-            <Comment name={"유다인"} comment={"나는 유다인이다!"}/>
-            <Comment name={"백승엽"} comment={"나는 백승여비다!"}/>
+            {comments.map((comment)=> {
+                return(
+                <Comment name={comment.name} comment={comment.comment} />
+                );
+            })}
         </div>
     );
 }
