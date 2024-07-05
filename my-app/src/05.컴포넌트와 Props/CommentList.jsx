b5bf9a82fd8f7097a3ed558304d74659e0c556c5
Yudain22 <di4046@gmail.com>
2024-07-05 11:44:51 +0900

    깃이그노얼

--- my-app/src/05.컴포넌트와 Props/CommentList.jsx
+++ my-app/src/05.컴포넌트와 Props/CommentList.jsx
@@ -4,7 +4,8 @@ import Comment from "./Comment";
 function CommentList(props) {
     return (
         <div>
-            <Comment />
+            <Comment name={"유다인"} comment={"나는 유다인이다!"}/>
+            <Comment name={"백승엽"} comment={"나는 백승여비다!"}/>
         </div>
     );
 }
