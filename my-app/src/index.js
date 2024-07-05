d8f9f4dbefa0beeb556fe3431ff0b06d1e5980ae
Yudain22 <di4046@gmail.com>
2024-07-05 11:00:19 +0900

    clock 예시

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,14 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './03.JSX소개/Library';
+import Clock from './04.엘리먼트 렌더링/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App /> */}
-    <Library/>
-  </React.StrictMode>
-);
+setInterval(()=> {
+  root.render(
+    <React.StrictMode>
+      {/* <App /> */}
+      {/* <Library/> */}
+      <Clock/>
+    </React.StrictMode>
+  );
+},1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
